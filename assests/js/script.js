14c5e4c73c001162c632a1fa4b185fbb03a08289
Laura Hofer <ljhofer@gmail.com>
2021-09-30 19:53:02 -0500

    Linked API fetch to button click, changed to imperial unit, added hide/show properties to main section, started writing variables for main section

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,8 +1,7 @@
 
 
-// Create a header with title
 
-// Create a side bar with input box, search button
+      
     //event listener
         //Save searched citites to local storage
         //Create button named that city 
@@ -10,21 +9,30 @@
 
 // Query the API for the city -- check if it is one request or two
     //Look inside each one to see what we need to pull out
-    //Add a dive for the current weather and populate it
+    //populate div for current wieather
         //Add classes based on current UV index 
         //Add images
-    //Add div of divs for the five-day forecast, populate it
+    //Populate div of divs for for the five-day forecast
         //Add images
 
-var searchBtn = $("city-search-button");
-var cityInputField = $("cityInput");
 
-var city = "";
+// Variables for aside
+var searchBtn = $("#city-search-button");
+var cityInputField = $("#cityInput")[0];
+
+// Variables for current weather section
+var mainSection = $("#main-section");
+var currentCityNow = $("current-city-now");
+var currentTemp = $("current-temp");
+var currentWind = $("current-wind");
+var currentHumid = $("current-humid");
+var currentUV = $("current-UV");
 
 
+// Fetches data from the API to populate the page
 function getAPI(city) {
     var APIKey = "06875dc6f6410e88cef926ae5d7a97b9";
-    var requestURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=06875dc6f6410e88cef926ae5d7a97b9";
+    var requestURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=06875dc6f6410e88cef926ae5d7a97b9";
 
     fetch(requestURL)
         .then(function(response){
@@ -32,15 +40,22 @@ function getAPI(city) {
         })
         .then(function(data) {
             console.log(data);
-        })
+            displayCurrentAPI();
 
+        })
 }
 
-getAPI();
+// function displayCurrentWeather(){
+//     currentCityNow.text()
 
-searchBtn.click(function() {
-    console.log(city);
-    city = cityInputField;
+
+// }
+
+// Event listener that listens for a button click, calls the function to get API, and displays main page section
+searchBtn.click(function(event) {
+    event.preventDefault();
+    var city = cityInputField.value;
+    mainSection.show();
     getAPI(city);
 
 })
